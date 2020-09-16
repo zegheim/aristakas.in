51c69c9ed81153f221d27220e3c2b0c99f31d8a0
zegheim <justinarista@gmail.com>
2020-09-16 20:28:24 +0100

    make icons larger on larger screen size

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const SocialIcons = () => (
-  <div>
+  <div className="md:text-lg lg:text-xl">
     <a href="https://linkedin.com/in/justinarista">
       <FontAwesomeIcon icon={faGithubSquare} className="mr-3" />
     </a>
