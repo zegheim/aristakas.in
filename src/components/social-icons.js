a742dd8d8a27bf6e513909df75cbf3b6fb717fda
zegheim <justinarista@gmail.com>
2020-09-18 23:38:08 +0100

    add aria label tags for accessibility purposes

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -3,11 +3,14 @@ import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const SocialIcons = () => (
   <div className="text-xl flex space-x-3">
-    <a href="https://github.com/zegheim">
-      <FontAwesomeIcon icon={faGithubSquare} />
+    <a aria-label="My GitHub Account" href="https://github.com/zegheim">
+      <FontAwesomeIcon icon={faGithubSquare} title="My GitHub Account" />
     </a>
-    <a href="https://linkedin.com/in/justinarista">
-      <FontAwesomeIcon icon={faLinkedin} />
+    <a
+      aria-label="My LinkedIn Account"
+      href="https://linkedin.com/in/justinarista"
+    >
+      <FontAwesomeIcon icon={faLinkedin} title="My LinkedIn Account" />
     </a>
   </div>
 );
