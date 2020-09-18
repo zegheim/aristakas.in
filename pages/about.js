fe847446d30371faac1f18fa79911fc0d22fa439
zegheim <justinarista@gmail.com>
2020-09-18 14:02:21 +0100

    add about page

---
+++ pages/about.js
@@ -0,0 +1,30 @@
+import Layout from "../src/components/layout";
+
+const AboutPage = () => (
+  <Layout pageName="About Me">
+    <section className="text-primary text-justify leading-snug px-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus urna
+        diam, varius eu sollicitudin vitae, pretium id ipsum. Integer
+        scelerisque molestie ipsum, et semper dui dictum accumsan. Vivamus ut
+        sollicitudin lorem. Mauris vitae dolor at velit vehicula tempus. Nulla
+        varius pellentesque ante sit amet suscipit. Sed in orci iaculis,
+        bibendum dui non, elementum eros. Maecenas cursus, dolor vel blandit
+        pharetra, nunc sem sollicitudin mauris, eget molestie magna magna in
+        velit.
+      </p>
+      <p>
+        Sed fringilla velit id ipsum pulvinar vestibulum. Duis a lorem eu metus
+        viverra porttitor eget at ante. Nullam eget ex in metus varius dignissim
+        eget vitae dolor. In malesuada vulputate nibh sed ultricies. Phasellus
+        eleifend auctor congue. In accumsan justo non accumsan venenatis. Nunc
+        molestie leo vitae est sodales malesuada. Quisque fermentum sollicitudin
+        est, vel lobortis orci posuere quis. Ut porta, nulla quis tincidunt
+        maximus, ex lacus pretium ante, ut lobortis nisi ligula a leo. Duis
+        volutpat erat quis tellus tincidunt sodales.
+      </p>
+    </section>
+  </Layout>
+);
+
+export default AboutPage;
