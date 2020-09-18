e047563c704f9c0f54e8657407cbc82be8168f2d
zegheim <justinarista@gmail.com>
2020-09-18 21:53:35 +0100

    use spacing utility instead of manually adding margin for social icon

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const SocialIcons = () => (
-  <div className="text-xl">
+  <div className="text-xl flex space-x-3">
     <a href="https://github.com/zegheim">
-      <FontAwesomeIcon icon={faGithubSquare} className="mr-3" />
+      <FontAwesomeIcon icon={faGithubSquare} />
     </a>
     <a href="https://linkedin.com/in/justinarista">
       <FontAwesomeIcon icon={faLinkedin} />
