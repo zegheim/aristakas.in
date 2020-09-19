94b8b13bc3437ca32a0e0034b6ccc5ee0c60306e
zegheim <justinarista@gmail.com>
2020-09-19 19:47:28 +0100

    refactored code

--- pages/blog.js
+++ pages/blog.js
@@ -2,24 +2,22 @@ import BlogCard from "../src/components/blog-card";
 import Layout from "../src/components/layout";
 import { getAllPostsForHome } from "../src/lib/api";
 
-const BlogPage = ({ allPosts }) => {
-  return (
-    <Layout pageName="Blog">
-      <div className="flex flex-col items-center space-y-4">
-        {allPosts.map(({ title, coverImage, created_at, slug, tags }) => (
-          <BlogCard
-            key={slug}
-            coverImage={coverImage}
-            slug={slug}
-            date={created_at}
-            title={title}
-            tags={tags}
-          />
-        ))}
-      </div>
-    </Layout>
-  );
-};
+const BlogPage = ({ allPosts }) => (
+  <Layout pageName="Blog">
+    <div className="flex flex-col items-center space-y-4">
+      {allPosts.map(({ title, coverImage, created_at, slug, tags }) => (
+        <BlogCard
+          key={slug}
+          coverImage={coverImage}
+          slug={slug}
+          date={created_at}
+          title={title}
+          tags={tags}
+        />
+      ))}
+    </div>
+  </Layout>
+);
 
 const getStaticProps = async () => {
   const allPosts = await getAllPostsForHome();
