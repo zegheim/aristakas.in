a3afb83b9030d0c9da5c9d0d844bb76e08c1479d
zegheim <justinarista@gmail.com>
2020-09-22 17:28:38 +0100

    finished writing about page

--- pages/about.js
+++ pages/about.js
@@ -3,25 +3,58 @@ import Layout from "../src/components/layout";
 const AboutPage = () => (
   <Layout pageName="About Me">
     <section className="text-primary text-justify leading-snug px-4">
+      <p>Hey there! 👋</p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus urna
-        diam, varius eu sollicitudin vitae, pretium id ipsum. Integer
-        scelerisque molestie ipsum, et semper dui dictum accumsan. Vivamus ut
-        sollicitudin lorem. Mauris vitae dolor at velit vehicula tempus. Nulla
-        varius pellentesque ante sit amet suscipit. Sed in orci iaculis,
-        bibendum dui non, elementum eros. Maecenas cursus, dolor vel blandit
-        pharetra, nunc sem sollicitudin mauris, eget molestie magna magna in
-        velit.
+        I am a Graduate Software Engineer at JP Morgan, as part of their{" "}
+        <a href="https://careers.jpmorgan.com/global/en/students/programs/software-engineer-fulltime">
+          Software Engineer Program (SEP)
+        </a>
+        . Before that, I studied Mathematics and Statistics as an undergraduate
+        at The University of Edinburgh, and graduated on July 2020. As of
+        September 2020, I have spent 5 years living in the UK!
       </p>
       <p>
-        Sed fringilla velit id ipsum pulvinar vestibulum. Duis a lorem eu metus
-        viverra porttitor eget at ante. Nullam eget ex in metus varius dignissim
-        eget vitae dolor. In malesuada vulputate nibh sed ultricies. Phasellus
-        eleifend auctor congue. In accumsan justo non accumsan venenatis. Nunc
-        molestie leo vitae est sodales malesuada. Quisque fermentum sollicitudin
-        est, vel lobortis orci posuere quis. Ut porta, nulla quis tincidunt
-        maximus, ex lacus pretium ante, ut lobortis nisi ligula a leo. Duis
-        volutpat erat quis tellus tincidunt sodales.
+        I am a{" "}
+        <a href="https://www.gnu.org/philosophy/free-sw.html">
+          free and open-source software (FOSS)
+        </a>{" "}
+        enthusiast, and one of my short-term goals is to be a FOSS contributor.
+        I use <a href="https://pop.system76.com/">Pop!_OS 20.04 LTS</a> running
+        the <a href="https://www.gnome.org/">GNOME desktop environment</a> as my
+        daily driver for personal development work, and my preferred text editor
+        is <a href="https://github.com/VSCodium/vscodium">VSCodium</a>, a
+        modified version of VSCode without the branding, intrusive telemetry,
+        and licensing requirements.
+      </p>
+      <p>
+        In the context of our increasingly digitalised world, digital privacy is
+        an issue that I feel strongly about and is one of the main reasons
+        behind my interest in FOSS. I am taking steps to minimise my digital
+        footprint, such as by using Firefox configured with{" "}
+        <a href="https://github.com/arkenfox/user.js">
+          <code>user.js</code>
+        </a>{" "}
+        and <a href="https://ublockorigin.com/">uBlock Origin</a>. There are
+        still{" "}
+        <a href="https://spyware.neocities.org/articles/index.html">
+          many aspects
+        </a>{" "}
+        where I fall short, but I hope to remedy them gradually.
+      </p>
+      <p>
+        As part of my commitment towards digital privacy, this website does not
+        employ any tracking measures, nor does it serve any cookies. This will
+        be good news to everyone, unless you&#39;re{" "}
+        <a href="https://muppet.fandom.com/wiki/Cookie_Monster">this guy</a>.
+        The only way for me to find out that you have visited this page is for
+        you to tell me directly.
+      </p>
+      <p>
+        I am always more than happy to talk about digital privacy and practical
+        steps you can take to minimise your footprint. Drop me an e-mail over at{" "}
+        <a href="mailto:hello@aristakas.in">hello@aristakas.in</a> if you want
+        to do just that! (Or just to say &#34;I visited your page!&#34; -
+        that&#39;s fine and much appreciated too.)
       </p>
     </section>
   </Layout>
