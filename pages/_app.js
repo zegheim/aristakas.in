1975c6d1bbb2e3935bae30e0cd2d9438e095ab4e
Justin Arista Kasin <justin+github@aristakas.in>
2021-10-04 22:47:37 +0100

    fix fontawesome bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "../src/styles/index.css";
+
+// For more info, see:
+// https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react#nextjs
+config.autoAddCss = false;
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 export default MyApp;
